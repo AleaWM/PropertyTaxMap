56c16871f41c4bf1885d4844484873dbc0e97f93
Alea Wilbur-Mujtaba <awilbu2@uic.edu>
2023-09-02 22:18:21 -0500

    Added 2nd version of app below 1st version. 2nd version has 2 layers tha work: percent C2 within each Muni and Composite tax rate for each township

--- app.R
+++ app.R
@@ -9,7 +9,6 @@
 
 library(shiny)
 library(sf)
-#install.packages("leaflet.extras")
 library(leaflet)
 library(leaflet.extras)
 library(tidyverse)
@@ -30,25 +29,35 @@ townships <- read_sf("https://gis.cookcountyil.gov/traditional/rest/services/pol
 
 #Tax_bill_deductions <- read_excel("~/PhD Fall 2021 - Spring 2022/Merriman RA/ptax/Tax bill deductions by grouped class_forAugust9.xlsx")
 
+nicknames <- readxl::read_excel("muni_shortnames.xlsx")
 
+#class_dict <- read_csv("class_dict_expanded.csv")
+
+TS_comprates <- read_csv("Township_Composite_Taxrates.csv") %>% 
+  arrange(short_name) 
+
+TS_joined <- left_join(townships, TS_comprates, by = c("NAME" = "short_name"))
+
+C2_w_HO_exe <- read_csv("C2_w_HOexe_takeuprate.csv")
+Munis_joined <- right_join(muni_shp, C2_w_HO_exe, by = c("MUNICIPALITY" = "clean_name"))
+
+
+elem_comprates <- read_csv("elem_schooldist_comprates.csv")
+
+# labels <- sprintf(
+#   "<strong>%s</strong><br/>%g people / mi<sup>2</sup>",
+#   nicknames$clean_name, C2_w_HO_exe$`2`
+# ) 
 
 # Define UI for application that draws a map
 ui <- fluidPage(
-  leafletOutput("map")
+  titlePanel("Cook County Property Tax Summaries"),
+    mainPanel(
+      leafletOutput("map")          
+      )
+  )
+
   
-)
-
-muni_shortnames <- read_excel("~/PhD Fall 2021 - Spring 2022/Merriman RA/ptax/muni_shortnames.xlsx")
-
-## Map exemption takeup rate for homeowners ## 
-
-C2_w_HO_exe <- read_csv("C2_w_HOexe_takeuprate.csv") %>% # made in 5_exemptionsScenarios.rmd
-  select(clean_name, `2`) %>% left_join(muni_shortnames) 
-
-TS_comprates <- read_csv("Township_Composite_Taxrates.csv") %>% left_join(muni_shortnames) 
-
-
-pal <- colorNumeric("viridis", NULL)
 
 
 # Define the server part of the Shiny app
@@ -69,18 +78,19 @@ server <- function(input, output, session) {
       # ) %>%
       # addPolygons(
       #   data = BOR_shp,
-      #   group = "BOR Boundary",
+      #   group = "Cook County",
+      #   layer = "Board of Review",
       #   options = pathOptions(
       #     fillOpacity = 0.2,
       #     color = "black",
       #     opacity = 1
       #   )
       # )  %>%
-      # 
-      # 
+
+
       addPolygons(
-        data = townships,
-        fillColor = pal(TS_comprates$tax_rate_current),
+        data = TS_joined,
+        fillColor = pal(TS_joined$tax_rate_current),
         label = ~NAME,  # Replace 'name' with the column name containing municipality names
         layerId = ~NAME,
         group = "Township Composite Tax Rates",
@@ -93,6 +103,7 @@ server <- function(input, output, session) {
       
       addPolygons(
         data = elem_schools_shp,
+        fillColor = pal(elem_comprates$tax_rate_current),
         label = ~AGENCY_DESCRIPTION,
         layerId = ~AGENCY_DESCRIPTION,
         group = "Elementary School Districts",
@@ -108,8 +119,10 @@ server <- function(input, output, session) {
       addPolygons(
         data = muni_shp,
         fillColor = pal(C2_w_HO_exe$`2`),
-        
         label = ~MUNICIPALITY,  # Replace 'name' with the column name containing municipality names
+    # style = list("font-weight" = "normal", padding = "3px 8px"),
+    # textsize = "15px",
+    # direction = "auto"))
         layerId = ~MUNICIPALITY,
         group = "Municipalities",
         options = pathOptions(
@@ -117,15 +130,16 @@ server <- function(input, output, session) {
           weight = 1,
           color = "black",
           opacity = 1,
-          fill = pal(C2_w_HO_exe$`2`)        )
+          fill = pal(C2_w_HO_exe$`2`)
+          )
       )  %>%
-      # addLegend("bottomright", pal = pal, values = ~TS_comprates$tax_rate_current,
-      #           title = "Current Composite Tax Rates",
-      #          # labFormat = labelFormat(prsefix = ""),
-      #           opacity = 1
-      # )
-      # 
+#       addLegend(position = "bottomright", pal = pal, values = ~C2_w_HO_exe$`2`,
+#                 title = "Current Composite Tax Rates",
+#                # labFormat = labelFormat(prsefix = ""),
+#                 opacity = 1
+#       ) %>%
 # 
+# # 
 #       addChoropleth(
 #         data = C2_w_HO_exe,
 #         valueProperty = `2`,  # Column name containing the values
@@ -142,19 +156,113 @@ server <- function(input, output, session) {
       #           title = "Takeup Rate",
       #           opacity = 1) %>%
       addLayersControl(  # Adding the layers control
-        overlayGroups = c( "Municipalities", "Township Composite Tax Rates",   "Elementary School Districts" 
+        overlayGroups = c( "Municipalities", "Township Composite Tax Rates",
+                           "Elementary School Districts" 
                          #  "BOR Boundary",  "Cook Boundary"
                          ),
+        
         options = layersControlOptions(collapsed = FALSE)
-      ) 
+      )
   })
+
 }
 
 # Run the Shiny app
 shinyApp(ui, server)
 
-#nicknames <- readxl::read_excel("muni_shortnames.xlsx")
 
-#class_dict <- read_csv("class_dict_expanded.csv")
+
+#########################33
+
+labels <- sprintf(
+  township_shp$NAME, TS_comprates$tax_rate_current
+) %>% lapply(htmltools::HTML)
+
+ui <- fluidPage(
+  titlePanel("Cook County Property Tax Summaries"),
+  mainPanel(
+    leafletOutput("map2")          
+  )
+)
+
+
+pal1 = colorBin("magma", domain = TS_joined$tax_rate_current , bins = 7)
+pal2 = colorBin("YlOrRd", domain = Munis_joined$`2` , bins = 7)
+
+# Define the server part of the Shiny app
+server <- function(input, output, session) {
+  output$map2 <- renderLeaflet({
+    leaflet() %>%
+      addProviderTiles("CartoDB.Positron") %>%
+      
+      addPolygons(
+        data = TS_joined,
+        fillColor = ~pal1(TS_joined$tax_rate_current),
+        label = ~NAME,  # Replace 'name' with the column name containing municipality names
+       #label = labels, 
+       layerId = ~NAME,
+       group = "Township Composite Tax Rates",
+       
+       # labelOptions = labelOptions(
+       #   style = list("font-weight" = "normal", padding = "3px 8px"),
+       #   textsize = "15px",
+       #   direction = "auto"),
+        options = pathOptions(
+          fillOpacity = 0.6,
+          weight = 1,
+          color = "black",
+          opacity = 1
+         )
+      #   highlightOptions = highlightOptions(
+      #     weight = 5,
+      #     color = "#666",
+      #     dashArray = "",
+      #     fillOpacity = 0.7,
+      #     bringToFront = TRUE)
+      ) %>%
+          addLegend(data = TS_joined,
+            position = "bottomright", pal = pal1,
+            values = ~TS_joined$tax_rate_current,
+            title = "Current Composite Tax Rates",
+            opacity = 1
+          )       %>%
+      
+      addPolygons(
+        data = Munis_joined,
+        fillColor = ~pal2(Munis_joined$`2`),
+        label = ~MUNICIPALITY,  # Replace 'name' with the column name containing municipality names
+        # style = list("font-weight" = "normal", padding = "3px 8px"),
+        # textsize = "15px",
+        # direction = "auto"))
+        layerId = ~MUNICIPALITY,
+        group = "Municipalities",
+        options = pathOptions(
+          fillOpacity = 0.6,
+          weight = 1,
+          color = "black",
+          opacity = 1
+        )) %>%
+      addLegend(data = Munis_joined,
+                position = "bottomleft", pal = pal2,
+                values = ~Munis_joined$`2`,
+                title = "% Residential",
+                opacity = 1
+      )       %>%
+      addLayersControl(  # Adding the layers control
+        overlayGroups = c( "Municipalities", "Township Composite Tax Rates"
+
+        ),
+        
+        options = layersControlOptions(collapsed = FALSE)
+      )
+      
+  })
+  
+}
+
+# Run the Shiny app
+shinyApp(ui, server)
+
+
 
 
